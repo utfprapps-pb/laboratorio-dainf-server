5dcd45667ae7c761ed6d34df556936a72288c041
Gustavo Zaffani <g_zaffani@hotmail.com>
2020-03-13 01:07:18 -0300

    realizado ajustes

--- src/main/java/br/com/utfpr/gerenciamento/server/controller/CompraController.java
+++ src/main/java/br/com/utfpr/gerenciamento/server/controller/CompraController.java
@@ -2,35 +2,20 @@ package br.com.utfpr.gerenciamento.server.controller;
 
 import br.com.utfpr.gerenciamento.server.model.Compra;
 import br.com.utfpr.gerenciamento.server.service.CompraService;
+import br.com.utfpr.gerenciamento.server.service.CrudService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("compra")
-public class CompraController {
+public class CompraController extends CrudController<Compra, Long> {
 
     @Autowired
     private CompraService compraService;
 
-    @GetMapping
-    public List<Compra> findAll() {
-        return compraService.findAll();
-    }
-
-    @GetMapping("{id}")
-    public Compra findOne(@PathVariable("id") Long id) {
-        return compraService.findOne(id);
-    }
-
-    @PostMapping
-    public Compra save(@RequestBody Compra compra) {
-        return compraService.save(compra);
-    }
-
-    @DeleteMapping("{id}")
-    public void delete(@PathVariable("id") Long id) {
-        compraService.delete(id);
+    @Override
+    protected CrudService<Compra, Long> getService() {
+        return compraService;
     }
 }
