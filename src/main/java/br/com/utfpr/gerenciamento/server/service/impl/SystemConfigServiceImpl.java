81a53ef82d3908ddccd47141dbe9baa9a887459c
geovanedcs <57137423+geovanedcs@users.noreply.github.com>
2025-10-13 19:44:46 -0300

    Update saveConfig method to initialize existingConfig with default ID

--- src/main/java/br/com/utfpr/gerenciamento/server/service/impl/SystemConfigServiceImpl.java
+++ src/main/java/br/com/utfpr/gerenciamento/server/service/impl/SystemConfigServiceImpl.java
@@ -23,7 +23,11 @@ public class SystemConfigServiceImpl implements SystemConfigService {
   @Override
   @Transactional
   public SystemConfig saveConfig(SystemConfig config) {
-    SystemConfig existingConfig = getConfig().orElse(new SystemConfig());
+    SystemConfig existingConfig = getConfig().orElseGet(() -> {
+      SystemConfig sc = new SystemConfig();
+      sc.setId(1L);
+      return sc;
+    });
     existingConfig.setNadaConstaEmail(config.getNadaConstaEmail());
     return repository.save(existingConfig);
   }
